bb25bc900512cd7db37516c6cbd77b2db2ebb0bc
Mohammad Waris <31559101+themdwaris@users.noreply.github.com>
2025-03-04 17:06:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,12 @@ connectCloudinary()
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+const corsOptions = {
+    origin: 'https://lookbymd.vercel.app', // Allow only this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+};
+app.use(cors(corsOptions))
 
 //api endpoints
 app.use("/api/user",userRouter)
